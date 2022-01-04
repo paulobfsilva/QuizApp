05470c8e425a4dc4ade7424e5836305c4ea81f15
Paulo Silva <paulobfsilva@gmail.com>
2022-01-04 18:14:30 +0000

    Fix issue with SceneDelegate when starting the app on the simulator

--- QuizApp/AppDelegate.swift
+++ QuizApp/AppDelegate.swift
@@ -14,15 +14,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISceneDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        let window = UIWindow(frame: UIScreen.main.bounds)
-        let viewController = QuestionViewController(question: "A question", options: ["Option 1", "Option 2"]) {
-            print($0)
-        }
-        _ = viewController.view
-        viewController.tableView.allowsMultipleSelection = true
-        window.rootViewController = viewController
-        self.window = window
-        window.makeKeyAndVisible()
         return true
     }
 
